dd8348884b4d7c9e86d228e12d1aa584b2a042a1
VladKyndiuk <vlad2013649@gmail.com>
2023-05-22 21:12:34 +0300

    update

--- controller/personController.js
+++ controller/personController.js
@@ -35,6 +35,14 @@ class personController {
     const users = await pool.query("select * from users");
     res.json(users.rows);
   }
+  async getUserInfo(req, res) {
+    const {id} = req.body;
+    const user = await pool.query("select * from users where id = $1", [
+      id
+    ]);
+
+
+  }
 
   async authorize(req, res) {
     const {email,password} = req.body;
@@ -59,11 +67,10 @@ class personController {
 
     return res.status(200).json({token});
   }
-//   async deletePerson(req, res) {
-//     const id = req.params.id;
-//     const deletedPerson = await pool.query("delete from person where person_id=$1",[id]);
-//     res.json(`USER ${id} DELETED`);
-//   }
+
+
+  
+
 }
 
 module.exports = new personController();
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,11 +5,15 @@ const personController = require("../controller/personController");
 
 // GET
 router.get("/persons", personController.getAllPersons);
+router.get("/get_user_info", personController.getUserInfo);
 
 
 
 router.post("/authorize", personController.authorize);
 router.post("/create_person", personController.createPerson);
+// router.post("/change_password", personController.changeUserPassword);
+
+
 // router.post("/delete_person/:id", personController.deletePerson);
 
 // UPDATE
